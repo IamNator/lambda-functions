5de3e840f7b95858716fc09c49cf345b147b3783
Nator Verinumbe <natverior1@gmail.com>
2023-04-16 00:11:21 +0100

    doc: add datadog logs

---
+++ cmd/email/main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"encoding/json"
+	"log"
+	"os"
+
+	"github.com/aws/aws-lambda-go/events"
+	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/sqs"
+	"github.com/DataDog/datadog-lambda-go/logs"
+
+)
+
+
+type EmailMessage struct {
+}
+
+
+func handler(ctx context.Context, msg EmailMessage) (string, error) {
+    logs.Log("Handling request")
+
+    // Your Lambda function logic here
+
+    logs.Log(fmt.Sprintf("Request completed"))
+
+    return "OK", nil
+}
+
+
+func main() {
+	lambda.Start(handler)
+}
